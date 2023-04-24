7a45618ef3eee6d8dd35094ee5bdecb2bb312b10
Omid Mokhtari <60341517+amisteromid@users.noreply.github.com>
2023-04-24 11:23:37 +0200

    Add files via upload

---
+++ 3_Alphafold_scores/foldx4/run_foldx/script.sh
@@ -0,0 +1,12 @@
+# loops through all directories in chains folder and run the FOLDX for each pdb file
+
+
+for pdb in /home/omid/Desktop/chains/*; do
+	pdb=$(echo $pdb |cut -d'/' -f6)
+	pair=$(echo $pdb |cut -d'/' -f6 |cut -d'.' -f1)
+	sed -e "s|pdb_file|$pdb|g;s|pair|$pair|g" config.cfg > modified_config.cfg
+	mkdir output/$pair
+	./foldx_20231231 -f modified_config.cfg
+	python3 /home/omid/Desktop/extract_foldx.py /home/omid/Downloads/foldx4/output/$pair/Interaction_* /home/omid/Downloads/foldx4/output/$pair
+done
+
